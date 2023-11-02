bd1499c803818e5d091cf4c085546c397e8a7dba
ariiana01 <104497804+ariiana01@users.noreply.github.com>
2023-11-02 16:53:45 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -109,15 +109,3 @@ function getStream (type) {
     });
 }
 
-<div class="columns">
-  <div class="column">
-    <p><button type="button" onclick="getStream('video')">Grab video</button></p>
-    
-    <video controls autoplay style="height:180px; width: 240px;"></video>
-  </div>
-  <div class="column">
-    <p><button type="button" onclick="getStream('audio')">Grab audio</button></p>
-    
-    <audio controls></audio>
-  </div>
-</div>
