e96e46cf3b786557af3f5a5daa715626c61d7d4f
ariiana01 <104497804+ariiana01@users.noreply.github.com>
2023-11-02 17:38:13 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,15 @@
 const container = document.querySelector(".container");
 
- 
-  
-
-if ("serviceWorker" in navigator) {
+ if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
     navigator.serviceWorker
-      .register("/serviceWorker.js") {scope: "./" }
+      .register("/serviceWorker.js") { scope: "./" }
       .then(res => console.log("service worker registered"))
       .catch(err => console.log("service worker not registered", err));
   });
-}
+  
+ }
+
 function getUserMedia(constraints) {
   // if Promise-based API is available, use it
   if (navigator.mediaDevices) {
